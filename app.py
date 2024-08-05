c8c2751ab92643aee3c6080921aaac012b783d20
aleks_3vs4n4y <aleksi.alatalo@gmail.com>
2024-08-05 22:04:43 +0300

    start developing new template, airthings not working

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
+import threading
+
 from flask import Flask, render_template, request, jsonify, send_from_directory
 import json
 import os
+import time
+from quart import Quart
+from ruuvitag_sensor.ruuvi import RuuviTagSensor
+import asyncio
 
 from pip._vendor import requests
 
@@ -58,35 +64,92 @@ def save_settings(settings):
     with open(SETTINGS_FILE, 'w') as file:
         json.dump(settings, file)
 
+last_fetch_time = 0
+cached_ruuvitag_data = {}
+async def ruuvitagData():
+    global last_fetch_time, cached_ruuvitag_data
+    current_time = time.time()
+    refresh_interval = 300  # 5 minutes
+
+    # Check if the data needs to be refreshed
+    if current_time - last_fetch_time < refresh_interval:
+        return cached_ruuvitag_data
+
+    conf = load_config()
+    macs = conf["macs"]
+    datas = dict()
+    res = {}
+    run = True
+    while run:
+        async for found_data in RuuviTagSensor.get_data_async(macs):
+            datas.update({found_data[0]: found_data[1]})
+            if len(datas.keys()) == 3:
+                run = False
+                break
+
+        for each in datas:
+            res[each] = datas[each]['temperature']
+
+    # Update the last fetch time and cached data
+    last_fetch_time = current_time
+    cached_ruuvitag_data = res
+
+    return res
+
+def schedule_ruuvitag_data():
+    asyncio.run(ruuvitagData())
+    threading.Timer(300, schedule_ruuvitag_data).start() # called every 5min
+
 def airthingsAuth():
     config=load_config()
+    current_time = time.time()
+    token_expiry_time =(config.get("airthingsAuthTokenExpiry", 0))
+    if current_time < token_expiry_time:
+        return  # Token is still valid, no need to refresh
+
     url="https://accounts-api.airthings.com/v1/token"
     headers = {
         "grant_type":"client_credentials",
         "client_id":config.get("airthingsClientId"),
         "client_secret":config.get("airthingsClientSecret"),
-        "scope": "read:device"
+        "Accept": "application/json"
         }
-    response = requests.post(url, headers=headers)
-    print(response.json())
+    response = requests.post(url, data=headers)   #headers should be in data?? and not headers?
     if response.status_code == 200:
         config["airthingsAuthToken"]=response.json().get("access_token")
+        config["airthingsAuthTokenExpiry"] = current_time + response.json().get("expires_in")
         save_config(config)
     else:
         print("Failed to authenticate with Airthings API")
 
+
 @app.route('/')
 def home():
-    #airthingsAuth()
     drawings = load_drawings()
     settings = load_settings()
     config=load_config()
     selected_templates = settings.get('selected_templates', ["", ""])
     return render_template('index.html', drawings=drawings, selected_templates=selected_templates, config=config)
 
+@app.route('/first')
+def home1():
+    drawings = load_drawings()
+    settings = load_settings()
+    config=load_config()
+    selected_templates = settings.get('selected_templates', ["", ""])
+    return render_template('index_first.html', drawings=drawings, selected_templates=selected_templates, config=config)
+
+
 @app.route('/<path:filename>')
 def serve_static(filename):
     return send_from_directory('static', filename)
+
+@app.route('/ruuvitag_data', methods=['GET'])
+async def get_ruuvitag_data():
+    res=await asyncio.create_task(ruuvitagData())
+    print(res)
+    return jsonify(res)
+
 
 
 @app.route('/draw')
@@ -95,6 +158,8 @@ def draw():
     color=settings.get('colors', {})
     rgba_colors = {color: hex_to_rgba(hex, 0.5) for color, hex in color.items()}
     return render_template('draw.html',colors=rgba_colors.values())
+
+
 
 @app.route('/settings')
 def settings():
@@ -168,5 +233,5 @@ def api_request(color):
         return jsonify(status='error', message='Failed to send API request'), response.status_code
 
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+if __name__ == "__main__":
+    app.run(debug=True, host="127.0.0.9", port=5000)
