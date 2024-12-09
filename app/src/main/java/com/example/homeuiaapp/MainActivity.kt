bcfa436c32319901f48b7ab291cecefb66291bca
aleks_3vs4n4y <aleksi.alatalo@gmail.com>
2024-12-09 20:47:56 +0200

    create adroid app

---
+++ app/src/main/java/com/example/homeuiaapp/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.homeuiaapp
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.View
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Button
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.core.view.WindowInsetsCompat
+import androidx.privacysandbox.tools.core.model.Type
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        setContentView(R.layout.activity_main)
+
+
+        val windowInsetsController =
+            WindowCompat.getInsetsController(window, window.decorView)
+
+        // Hide the system bars.
+        windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
+
+
+        val openWebsiteButton: Button = findViewById(R.id.openWebsiteButton)
+        val webView: WebView = findViewById(R.id.webView)
+
+
+        // Set up WebView
+        webView.settings.apply {
+            javaScriptEnabled = true
+            cacheMode = WebSettings.LOAD_NO_CACHE
+        }
+        webView.setInitialScale(100)
+        webView.webViewClient = WebViewClient()
+        val websiteUrl = "http://192.168.69.185:5000"
+        webView.loadUrl(websiteUrl)
+
+        webView.visibility = View.VISIBLE
+        openWebsiteButton.visibility = View.GONE
+
+
+    }
+
+}
